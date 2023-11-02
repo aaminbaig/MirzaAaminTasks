e413b81f9919942f60f8bef920a90fdc87c2f71e
mirza aamin <mirza.aamin@codeninjaconsulting.com>
2023-11-02 14:38:11 +0500

    1714: basic web api crud

---
+++ Task2/TaskDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class TaskDbContext : DbContext
+{
+    public TaskDbContext(DbContextOptions<TaskDbContext> contextOptions) : base(contextOptions) { }
+
+    public DbSet<Todo> TodoTable { get; set; }
+}
+
